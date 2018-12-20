a2852a65645e90be687a9a5a5a9b77703181360c
Vanessa Sochat <vsochat@stanford.edu>
2018-12-20 11:09:51 -0500

    bumping version

--- schemaorg/version.py
+++ schemaorg/version.py
@@ -17,7 +17,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 '''
 
-__version__ = "0.0.17"
+__version__ = "0.0.18"
 AUTHOR = 'Vanessa Sochat'
 AUTHOR_EMAIL = 'vsochat@stanford.edu'
 NAME = 'schemaorg'
