95f9fd2edb3be037d8b3cbc2d580a0b748afaa42
vsoch <vsoch@users.noreply.github.com>
2021-04-07 08:13:22 -0600

    forgot to commit these changes
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- schemaorg/version.py
+++ schemaorg/version.py
@@ -1,27 +1,12 @@
-'''
+__author__ = "Vanessa Sochat"
+__copyright__ = "Copyright 2018-2021, Vanessa Sochat"
+__license__ = "MPL 2.0"
 
-Copyright (C) 2018-2020 Vanessa Sochat.
-
-This program is free software: you can redistribute it and/or modify it
-under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or (at your
-option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
-License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-'''
-
-__version__ = "0.0.24"
+__version__ = "0.1.0"
 AUTHOR = 'Vanessa Sochat'
-AUTHOR_EMAIL = 'vsochat@stanford.edu'
+AUTHOR_EMAIL = '@vsoch'
 NAME = 'schemaorg'
-PACKAGE_URL = "http://www.github.com/openschemas/schemaorg"
+PACKAGE_URL = "https://github.com/openschemas/schemaorg"
 KEYWORDS = 'openschemas, schema.org'
 DESCRIPTION = "Python functions for applied use of schema.org"
 LICENSE = "LICENSE"
